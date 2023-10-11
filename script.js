97824c416a02e19641f1a2437da5ea9012d97a9e
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-10-11 16:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here
+const input = document.querySelector('#newTodoInput');
+const btn = document.querySelector('#addTodoBtn');
+const list = document.querySelector('#todoList');
 
+btn.addEventListener('click', ()=>{
+	if(!input.value.trim()) return;
+
+	const data = input.value.trim();
+
+	const li = document.createElement('li');
+	li.innerText = data;
+	li.addEventListener('click', getData);
+	list.appendChild(li);
+})
+
+function getData(e){
+	const data = e.target.innerText;
+	input.value = data;
+}
